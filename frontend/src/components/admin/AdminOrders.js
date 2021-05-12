f0968de0e2b331c8d142b36fc43a31ef276231e1
add <jalloh@jalloh.tech>
2021-05-12 13:56:09 +0300

    adminOrderComponent bug fixed

--- frontend/src/components/admin/AdminOrders.js
+++ frontend/src/components/admin/AdminOrders.js
@@ -20,17 +20,13 @@ const AdminOrders = (props) => {
                 <AdminHomePageHeader />
             </div>
 
-
             <h1 className='ui container'>ORDERS</h1>
             <div className="ui stackable grid container">
                 <div className="sixteen wide column">
 
                     <div className="column">
                         {
-                            loading ? <Loader /> : null
-                        }
-                        {
-                            error ? <Error /> : (
+                            loading ? <Loader /> : (
                                 <Fragment>
                                     <table className="ui celled table">
                                         <thead>
@@ -76,6 +72,53 @@ const AdminOrders = (props) => {
                                 </Fragment>
                             )
                         }
+                        {/*{*/}
+                        {/*    error ? <Error /> : (*/}
+                        {/*        <Fragment>*/}
+                        {/*            <table className="ui celled table">*/}
+                        {/*                <thead>*/}
+                        {/*                <tr>*/}
+                        {/*                    <th>ID</th>*/}
+                        {/*                    <th>USER</th>*/}
+                        {/*                    <th>DATE</th>*/}
+                        {/*                    <th>TOTAL</th>*/}
+                        {/*                    <th>PAID</th>*/}
+                        {/*                    <th>DELIVERED</th>*/}
+                        {/*                    <th>{}</th>*/}
+                        {/*                </tr>*/}
+                        {/*                </thead>*/}
+                        {/*                {*/}
+                        {/*                    orders.map((order) => {*/}
+                        {/*                        return(*/}
+                        {/*                            <Fragment key={order._id}>*/}
+                        {/*                                <tbody>*/}
+                        {/*                                <tr>*/}
+                        {/*                                    <td>{order._id}</td>*/}
+                        {/*                                    <td>{order.user.name}</td>*/}
+                        {/*                                    <td>{order.createdAt.substr(0, 10)}</td>*/}
+                        {/*                                    <td>${order.totalPrice.toFixed(2)}</td>*/}
+                        {/*                                    <td>{order.isPaid && order.paymentResult.update_time.substr(0, 10)}</td>*/}
+                        {/*                                    <td>{order.isDelivered ?*/}
+                        {/*                                        <i style={{color: 'green'}} className="check circle icon">{}</i> :*/}
+                        {/*                                        (<i className="x icon">{}</i>)}*/}
+                        {/*                                    </td>*/}
+
+                        {/*                                    <td>*/}
+                        {/*                                        <Link to={{pathname:`/orders/${order._id}/details`, data: order._id}}>*/}
+                        {/*                                            <button className="ui black button" type="button">Details</button>*/}
+                        {/*                                        </Link>*/}
+
+                        {/*                                    </td>*/}
+                        {/*                                </tr>*/}
+                        {/*                                </tbody>*/}
+                        {/*                            </Fragment>*/}
+                        {/*                        )*/}
+                        {/*                    })*/}
+                        {/*                }*/}
+                        {/*            </table>*/}
+                        {/*        </Fragment>*/}
+                        {/*    )*/}
+                        {/*}*/}
 
                     </div>
                 </div>
